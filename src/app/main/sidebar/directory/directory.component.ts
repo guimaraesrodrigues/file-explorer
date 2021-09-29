2387e0d296696717e011273cf8d30aa086bf77a1
Rodrigo Guimarães <guimaraes_rodrigues@yahoo.com>
2021-09-29 15:17:43 -0300

    show file icons

--- src/app/main/sidebar/directory/directory.component.ts
+++ src/app/main/sidebar/directory/directory.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Icons } from 'src/app/shared/enums/icons.enums';
 import { TreeNode } from 'src/app/shared/models/tree-node.model';
 import { IconService } from 'src/app/shared/services/icon/icon.service';
 
@@ -23,8 +24,13 @@ export class DirectoryComponent implements OnInit {
   }
 
   public setCollapsed(): void {
-    console.log('oiiii', this.collapsed);
     this.collapsed = !this.collapsed;
+  }
+
+  public getExtension(fileName: string): string {
+    const extension = Icons[fileName.split('.').pop()];
+
+    return extension ? extension : Icons.default;
   }
 
 }
--- src/app/shared/enums/icons.enums.ts
+++ src/app/shared/enums/icons.enums.ts
@@ -1,3 +1,13 @@
 export enum Icons {
   ArrowDown = 'arrowDown',
+  ArrowRight = 'arrowRight',
+  x = 'x',
+  css = 'cssFile',
+  default = 'defaultFile',
+  git = 'gitFile',
+  ico = 'imageFile',
+  js = 'jsFile',
+  json = 'jsonFile',
+  md = 'readmeFile',
+  yarn = 'yarnFile'
 }
